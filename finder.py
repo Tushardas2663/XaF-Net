ce5a3f65d66a1471bf28090352bc3f974ec4d418
Tushardas2663 <147297285+Tushardas2663@users.noreply.github.com>
2026-01-05 00:47:44 +0530

    params
    Added model checkpointing and training details.

--- finder.py
+++ finder.py
@@ -14,3 +14,25 @@ def build_finder_cnn(input_shape):
         layers.Dense(1, activation='sigmoid') 
     ])
      return model
+
+"""Important training details:
+
+checkpoint_filepath = 'best_model_checkpoint.weights.h5'
+model_checkpoint_callback = ModelCheckpoint(
+    filepath=checkpoint_filepath,save_weights_only=True,
+    monitor='val_accuracy',  
+    mode='max',     
+    save_best_only=True, 
+    verbose=1            
+)
+
+
+finder_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+
+finder_history = finder_model.fit(X_finder_train, y_finder_train,
+                                epochs=20, 
+                                batch_size=32,
+                                validation_data=(X_finder_val, y_finder_val),callbacks=[model_checkpoint_callback],
+                                verbose=1)
+"""
