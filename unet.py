f307388af810523a67b7f7bae7f6e01ef44ff408
Tushardas2663 <147297285+Tushardas2663@users.noreply.github.com>
2026-01-04 23:14:57 +0530

    unet

---
+++ unet.py
@@ -0,0 +1,14 @@
+def build_simple_unet(input_shape):
+
+    inputs = keras.Input(shape=input_shape)
+    c1 = layers.Conv2D(16, (3, 3), activation='relu', padding='same')(inputs)
+    p1 = layers.MaxPooling2D((2, 2))(c1); c2 = layers.Conv2D(32, (3, 3), activation='relu', padding='same')(p1)
+    p2 = layers.MaxPooling2D((2, 2))(c2); c3 = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(p2)
+    u4 = layers.Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c3); u4 = layers.concatenate([u4, c2])
+    c4 = layers.Conv2D(32, (3, 3), activation='relu', padding='same')(u4)
+    u5 = layers.Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c4); u5 = layers.concatenate([u5, c1])
+    c5 = layers.Conv2D(16, (3, 3), activation='relu', padding='same')(u5)
+    outputs = layers.Conv2D(1, (1, 1), activation='sigmoid')(c5)
+    model = keras.Model(inputs, outputs); return model
+
+
